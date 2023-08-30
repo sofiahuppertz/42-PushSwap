a93f39ebe121e78de8daa96a4c9dbeb22161f089
Sofía Huppertz <sofia.huppertz@gmail.com>
2023-08-30 17:24:42 +0200

    sorts

--- push_swap.h
+++ push_swap.h
@@ -16,6 +16,7 @@
 # include "42-Libft/libft/ft_printf.h"
 # include "42-Libft/libft/libft.h"
 # include <limits.h>
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -26,6 +27,8 @@ typedef struct s_data
 	int id;
 	int pos; 
 	int assoc_pos; 
+	int moves_a;
+	int moves_b;
 	int cost;	
 }					t_data; 
 
@@ -43,18 +46,19 @@ void delete_lst(t_data **lst, void (*del)(void *));
 void delete_str_arr(char **str_arr);
 void do_cheapest_move(t_data **stack_a, t_data **stack_b, t_data *min_cost_value);
 void   error(t_data **list);
-void	extract_per_bound(t_data **stack_a, t_data **stack_b, int bound, int size);
-void insert_idx(t_data **stack_b, int *sorted, int size);
+void	extract_per_bound(t_data **stack_a, t_data **stack_b, int bound);
+void insert_id(t_data **stack_a, int *sorted, int size);
 void	insert_node(t_data **lst, t_data *new_lst);
 void insert_num(int *num, char *temp, t_data **stack);
 void	minimum_cost_sort(t_data **stack_a, t_data **stack_b);
 void set_info(t_data *current, t_data **stack_a, t_data **stack_b);
-void sort_copy(t_data **stack_a, t_data **stack_b, int *sorted_copy, int size);
+void sort_copy(t_data **stack_a, t_data **stack_b, int **sorted_copy, int size);
 void	swap(int *a, int *b);
 void    swap_a(t_data **stack_a, int print);
 void    swap_a_and_b(t_data **stack_a, t_data **stack_b);
 void swap_b(t_data **stack_b, int print);
 void parse(int argc, char *argv[], t_data **stack);
+void perform_rotations(t_data **stack_a, t_data **stack_b, int moves_a, int moves_b);
 void push_a(t_data **stack_b, t_data **stack_a);
 void push_b(t_data **stack_a, t_data **stack_b);
 void    push_swap(t_data **stack_a, t_data **stack_b);
@@ -69,9 +73,11 @@ void rotate_b(t_data **stack_b, int print);
 void rotate_a_and_b(t_data **stack_a, t_data **stack_b);
 void trio_sort(t_data **stack_a);
 
-int calculate_cost(t_data *current, t_data *stack_a, t_data  *stack_b);
+int calculate_cost(t_data *current, int size_a, int size_b);
 int check_reversed_top(t_data **stack);
+int 	ft_abs(int n);
 int	get_max(t_data **stack);
+int		get_min(t_data **stack);
 int get_min_idx(t_data **stack_a);
 int	get_size(t_data *lst);
 int is_numeric(char *str);
@@ -81,6 +87,7 @@ int sorted(t_data **stack_a);
 
 
 t_data	*create_node(void *content);
+t_data 	*get_min_cost_value(t_data *stack_b);
 
 #endif
 
--- srcs/QueenOfHeartSort.c
+++ srcs/QueenOfHeartSort.c
@@ -19,64 +19,354 @@ void	big_sort(t_data **stack_a, t_data **stack_b, int size)
 	int i;
 
 	sorted_copy = NULL;
-	sort_copy(stack_a, stack_b, sorted_copy, size);
-	insert_idx(stack_b, sorted_copy, size);
+	sort_copy(stack_a, stack_b, &sorted_copy, size);
+	insert_id(stack_a, sorted_copy, size);
 	calculate_quantile_bounds(quantile_bounds, sorted_copy, size);
 	i = 0;
 	while (i < 5)
 	{
-		extract_per_bound(stack_a, stack_b, quantile_bounds[i], size);
+		extract_per_bound(stack_a, stack_b, quantile_bounds[i]);
 		i++;
 	}
 	minimum_cost_sort(stack_a, stack_b);
-
 	free(sorted_copy);
 }
 
-void calculate_quantile_bounds(int quantile_bounds[5], int *sorted_copy, int size)
+void	minimum_cost_sort(t_data **stack_a, t_data **stack_b)
 {
-	int quantile;
-	int i;
+	t_data	*current;
+	int size_a;
+	int size_b;
+	t_data	*min_cost_value;
 
-	i = 1;
-	while (i < 6)
+	while (*stack_b)
 	{
-		quantile = (size -1) * (i / 5);
-		quantile_bounds[i - 1] = sorted_copy[quantile];
-		i++;
+		current = *stack_b;
+		while (current)
+		{
+			size_a = get_size(*stack_a);
+			size_b = get_size(*stack_b);
+			set_info(current, stack_a, stack_b);
+			current->cost = calculate_cost(current, size_a, size_b);
+			current = current->next;
+		}
+		min_cost_value = get_min_cost_value(*stack_b);
+		do_cheapest_move(stack_a, stack_b, min_cost_value);
+	}	
+}
+
+void do_cheapest_move(t_data **stack_a, t_data **stack_b, t_data *min_cost_value)
+{
+	if (min_cost_value->cost == 0)
+	{
+		push_a(stack_b, stack_a);
+		return;
+	}
+	perform_rotations(stack_a, stack_b, min_cost_value->moves_a, min_cost_value->moves_b);
+	push_a(stack_b, stack_a);
+	return;
+}
+
+void perform_rotations(t_data **stack_a, t_data **stack_b, int moves_a, int moves_b)
+{
+
+	if (moves_a >= 0 && moves_b >= 0)
+	{
+		if (moves_a < moves_b)
+		{
+			while (moves_a > moves_b)
+			{
+				rotate_a(stack_a, 1);
+				moves_a -= 1;
+			}	
+			while (moves_b > 0)
+			{
+				rotate_a_and_b(stack_a, stack_b);
+				moves_b -= 1;
+			}
+				
+		}
+		else
+		{
+			while (moves_b > moves_a)
+			{
+				rotate_b(stack_b, 1);
+				moves_b += 1;
+			}	
+			while (moves_a > 0)
+			{
+				rotate_a_and_b(stack_a, stack_b);
+				moves_a -= 1;
+			}	
+		}	
+	}
+	else if (moves_a <= 0  && moves_b <= 0)
+	{
+		if (moves_a < moves_b)
+		{
+			while (moves_a < moves_b)
+			{
+				reverse_rotate_a(stack_a, 1);
+				moves_a += 1;
+			}
+			while (moves_b < 0)
+			{
+				reverse_rotate_a_and_b(stack_a, stack_b);
+				moves_b += 1;
+			}
+		}
+		else
+		{
+			while (moves_a > moves_b)
+			{
+				reverse_rotate_b(stack_b, 1);
+				moves_b += 1;
+			}
+			while (moves_b < 0)
+			{
+				reverse_rotate_a_and_b(stack_a, stack_b);
+				moves_b += 1;
+			}
+		}
+	}
+	else 
+	{
+		while (moves_a > 0)
+		{
+			rotate_a(stack_a, 1);
+			moves_a -= 1;
+		}
+		while (moves_a < 0)
+		{
+			reverse_rotate_a(stack_a, 1);
+			moves_a += 1;
+		}
+		while (moves_b > 0)
+		{
+			rotate_a(stack_a, 1);
+			moves_b -= 1;
+		}
+		while (moves_b < 0)
+		{
+			reverse_rotate_a(stack_a, 1);
+			moves_b += 1;
+		}
 	}
 }
 
+t_data 	*get_min_cost_value(t_data *stack_b)
+{
+	t_data *min_cost_value;
+	t_data *ptr;
+	ptr = stack_b;
+	min_cost_value = ptr;
+	while (ptr)
+	{
+		if (ft_abs(ptr->cost) < ft_abs(min_cost_value->cost))
+			min_cost_value = ptr;
+		ptr = ptr->next;
+	}
+	return (min_cost_value);
+}
 
-void sort_copy(t_data **stack_a, t_data **stack_b, int *sorted_copy, int size)
+int 	ft_abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
+
+int calculate_cost(t_data *current, int size_a, int size_b)
+{
+	int cost;
+
+	if (current->pos > size_b / 2)
+		current->moves_b = -1 * (size_b - current->pos);
+	else
+		current->moves_b = current->pos;
+	
+	if (current->assoc_pos > size_a / 2)
+		current->moves_a = -1 * (size_a - current->assoc_pos);
+	else
+		current->moves_a = current->assoc_pos;
+	if (current->moves_b <= 0 && current->moves_a <= 0)
+	{
+		if (current->moves_a < current->moves_b)
+			cost = current->moves_a;
+		else
+			cost = current->moves_b;
+	}
+	else if (current->moves_b >= 0 && current->moves_b >= 0)
+		if (current->moves_a < current->moves_b)
+			cost = current->moves_b;
+		else
+			cost = current->moves_a;
+	else
+		cost = abs(current->moves_a) + abs(current->moves_b);
+	return (cost);
+}
+
+void set_info(t_data *current, t_data **stack_a, t_data **stack_b)
+{
+	int i;
+	int min_a;
+	t_data *previous;
+	t_data *ptr;
+
+
+// 1. Set pos.
+	ptr = *stack_b;
+	i = 0;
+	while (ptr)
+	{
+		if (ptr->id == current->id)
+		{
+			current->pos = i;
+			break ;
+		}
+		i++;
+		ptr = ptr->next;
+	}
+// 2. Set assoc_pos.
+	i = 0;
+	min_a = get_min(stack_a);
+	if (min_a > *(int *)current->content)
+		current->assoc_pos = get_min_idx(stack_a);
+	else
+	{
+		i = 0;
+		while (ptr)
+		{
+			previous = ptr;
+			ptr = ptr->next;
+			if (*(int *)ptr->content > *(int *)current->content && *(int *)previous->content < *(int *)current->content)
+			{
+				current->assoc_pos = i;
+				break;
+			}
+			i += 1;
+		}
+	}	
+}
+
+int		get_min(t_data **stack)
+{
+	t_data	*ptr;
+	int		min;
+
+	ptr = *stack;
+	min = *(int *)ptr->content;
+	while (ptr)
+	{
+		if (*(int *)ptr->content < min)
+			min = *(int *)ptr->content;
+		ptr = ptr->next;
+	}
+	return (min);
+}
+
+
+void	extract_per_bound(t_data **stack_a, t_data **stack_b, int bound)
+{
+	int len;
+	int index;
+	int first_hold;
+	int last_hold;
+	t_data *ptr;
+
+	index = 0;
+	first_hold = -1;
+	last_hold = -1;
+	len = get_size(*stack_a);
+	ptr = *stack_a;
+	while (len > 3)
+	{
+		while (ptr)
+		{
+			if (*(int *)ptr->content < bound)
+			{
+				if (first_hold == -1)
+					first_hold = index;
+				last_hold = index;
+			}
+			index += 1;
+			ptr = ptr->next;
+		}
+		if (first_hold == -1)
+			return;
+		else if (first_hold < len - last_hold)
+		{
+			while (first_hold > 0)
+			{
+				rotate_a(stack_a, 1);
+				first_hold -= 1;
+			}
+		}
+		else 
+		{
+			while (last_hold < len)
+			{
+				reverse_rotate_a(stack_a, 1);
+				last_hold += 1;
+			}
+		}
+		push_b(stack_a, stack_b);
+		ptr = *stack_a;
+		index = 0;
+		first_hold = -1;
+		last_hold = -1;
+		len -= 1;
+	}
+	if (len == 3)
+		trio_sort(stack_a);
+	else if (len == 2 && !(sorted(stack_a)))
+		swap_a(stack_a, 1);
+}
+
+
+void calculate_quantile_bounds(int quantile_bounds[5], int *sorted_copy, int size)
+{
+	int i;
+
+	i = 0;
+	while (i < 4)
+	{
+		quantile_bounds[i] = sorted_copy[size / 5 + i * (size / 5)];
+		i++;
+	}
+	quantile_bounds[4] = sorted_copy[size - 1];
+}
+
+
+void sort_copy(t_data **stack_a, t_data **stack_b, int **sorted_copy, int size)
 {
 	t_data	*current;
 	int i;
 	
 	i = 0;
-	sorted_copy = malloc(sizeof(int) * size);
-	if (!sorted_copy)
+	*sorted_copy = malloc(sizeof(int) * size);
+	if (!(*sorted_copy))
 	{
 		delete_lst(stack_a, free);
 		delete_lst(stack_b, free);
 		exit(1);
 	}
-		current = *stack_b;
-	while (current)
+	current = *stack_a;
+	while (current && i < size)
 	{
-		sorted_copy[i] = *(int *)current->content;
+		(*sorted_copy)[i] = *(int *)current->content;
 		current = current->next;
 		i += 1;
 	}
-	quicksort(sorted_copy, 0, size - 1);
+	quicksort(*sorted_copy, 0, size - 1);
 }
 
-void insert_idx(t_data **stack_b, int *sorted, int size)
+void insert_id(t_data **stack_a, int *sorted, int size)
 {
 	t_data	*current;
 	int i;
 
-	current = *stack_b;
+	current = *stack_a;
 	while (current)
 	{
 		i = 0;
@@ -92,257 +382,4 @@ void insert_idx(t_data **stack_b, int *sorted, int size)
 		current = current->next;
 	}
 }
-
-void	minimum_cost_sort(t_data **stack_a, t_data **stack_b)
-{
-	t_data	*current;
-	t_data	*min_cost_value;
-
-	while (*stack_a)
-	{
-		current = *stack_b;
-		min_cost_value = current;
-		min_cost_value->cost = calculate_cost(min_cost_value, *stack_a, *stack_b);
-		while (current)
-		{
-			set_info(current, stack_a, stack_b);
-			current->cost = calculate_cost(current, *stack_a, *stack_b);
-			if (current->cost < min_cost_value->cost)
-				min_cost_value = current;
-			current = current->next;
-		}
-		do_cheapest_move(stack_a, stack_b, min_cost_value);
-	}
-}
-
-void do_cheapest_move(t_data **stack_a, t_data **stack_b, t_data *min_cost_value)
-{
-	int size_a;
-	int size_b;
-	int cost_a;
-	int cost_b;
-
-	size_a = get_size(*stack_a);
-	size_b = get_size(*stack_b);
-
-	if (min_cost_value->pos < size_b / 2)
-	{
-		cost_b = -(size_b - min_cost_value->pos);
-	}
-	else
-	{
-		cost_b = size_b - min_cost_value->pos;
-	}
-	if (min_cost_value->assoc_pos < size_a / 2)
-		cost_a = -(size_a - min_cost_value->assoc_pos);
-	else
-		cost_a = size_a - min_cost_value->assoc_pos;
-	
-
-	int i = 0;
-	if (cost_b < 0 && cost_b < 0)
-	{
-		if (cost_a < cost_b)
-		{
-			while (i < -(cost_b))
-			{
-				reverse_rotate_a_and_b(stack_a, stack_b);
-				i += 1;
-			}
-			while (i < cost_a)
-			{
-				reverse_rotate_a(stack_a, 1);
-				i += 1;
-			}
-			push_a(stack_b, stack_a);	
-		}
-		else
-		{
-			while (i < -(cost_a))
-			{
-				reverse_rotate_a_and_b(stack_a, stack_b);
-				i += 1;
-			}
-			while (i < cost_b)
-			{
-				reverse_rotate_b(stack_a, 1);
-				i += 1;
-			}
-			push_a(stack_b, stack_a);	
-		}
-			
-	} 
-	else if (cost_b > 0 && cost_b > 0)
-	{
-		if (cost_a > cost_b)
-		{
-			while (i < cost_b)
-			{
-				rotate_a_and_b(stack_a, stack_b);
-				i += 1;
-			}
-			while (i < cost_a)
-			{
-				rotate_a(stack_a, 1);
-				i += 1;
-			}
-			push_a(stack_b, stack_a);	
-		}
-		else
-		{
-			while (i < cost_a)
-			{
-				rotate_a_and_b(stack_a, stack_b);
-				i += 1;
-			}
-			while (i < cost_b)
-			{
-				rotate_b(stack_a, 1);
-				i += 1;
-			}
-			push_a(stack_b, stack_a);
-		}
-	}
-	else
-	{
-		if (cost_a < 0)
-		{
-			while (i < -(cost_a))
-			{
-				reverse_rotate_a(stack_a, 1);
-				i += 1;
-			}
-		}
-		else
-		{
-			while (i < cost_a)
-			{
-				rotate_a(stack_a, 1);
-				i += 1;
-			}
-		}
-		i = 0;
-		if (cost_b < 0)
-		{
-			while (i < -(cost_b))
-			{
-				reverse_rotate_b(stack_b, 1);
-				i += 1;
-			}
-		}
-		else 
-		{
-			while (i < cost_b)
-			{
-				rotate_b(stack_b, 1);
-				i += 1;
-			}
-		}
-		push_a(stack_b, stack_a);
-	}
-
-}
-
-int calculate_cost(t_data *current, t_data *stack_a, t_data  *stack_b)
-{
-	int cost;
-	int cost_b;
-	int cost_a;
-	int size_a;
-	int size_b;
-
-	size_a = get_size(stack_a);
-	size_b = get_size(stack_b);
-
-	if (current->pos < size_b / 2)
-		cost_b = -(size_b - current->pos);
-	else
-		cost_b = size_b - current->pos;
-	
-	if (current->assoc_pos < size_a / 2)
-		cost_a = -(size_a - current->assoc_pos);
-	else
-		cost_a = size_a - current->assoc_pos;
-	if (cost_b < 0 && cost_b < 0)
-		cost = cost_b > cost_a ? cost_a : cost_b; //smalles is biggest in negatives
-	else if (cost_b > 0 && cost_b > 0)
-		cost = cost_b < cost_a ? cost_a : cost_b;
-	else
-		cost = abs(cost_a) + abs(cost_b);
-	return (cost + 1); //+1 for the pa.
-}
-
-void set_info(t_data *current, t_data **stack_a, t_data **stack_b)
-{
-	int i;
-	int j;
-	t_data *ptr;
-	t_data *first_b;
-
-	ptr = *stack_a;
-	i = 0;
-	while (ptr)
-	{
-		if (ptr->id == current->id)
-		{
-			current->pos = i;
-			break ;
-		}
-		i++;
-		ptr = ptr->next;
-	}
-	current->assoc_pos = -1;
-	first_b = *stack_b;
-	ptr = *stack_b;
-	j = 0;
-	while (ptr)
-	{
-		if (ptr->id < current->id)
-		{
-			if (ptr->next->id > current->id)	
-			{
-				current->assoc_pos = j + 1;
-				break ;
-			}
-			else if (ptr->next == NULL && first_b->id > current->id)
-			{
-				current->assoc_pos = 1;
-				break ;
-			}
-			ptr = ptr->next;
-			j++;
-		}	
-	}
-	if (current->assoc_pos == -1)
-		current->assoc_pos = get_min_idx(stack_b);
-}
-
-void	extract_per_bound(t_data **stack_a, t_data **stack_b, int bound, int size)
-{
-	int		n;
-	t_data	*top;
-
-	n = size;
-	while (n > 3)
-	{
-		if (sorted(stack_a))
-			return ;
-		top = *stack_a;
-		if (*(int *)top->content < bound)
-			push_b(stack_a, stack_b);
-		else
-			rotate_a(stack_a, 1);
-		n--;
-	}
-	if (size == 3)
-		trio_sort(stack_a);
-	if (size == 2 && !(sorted(stack_a)))
-	{
-		if (check_reversed_top(stack_b))
-			swap_a_and_b(stack_a, stack_b);
-		else
-			swap_a(stack_a, 1);
-	}
-}
-
 
